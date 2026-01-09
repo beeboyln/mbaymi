ad2b22d3d52942ab5cde6b43be11157f8f049823
llmnd <papendiaye511@gmail.com>
2026-01-09 09:56:27 +0000

    following

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,5 +4,6 @@ from .livestock import Livestock
 from .market import MarketPrice
 from .crop_problem import CropProblem
 from .farm_network import FarmProfile, FarmPost, FarmFollowing
+from .user_following import UserFollowing
 
-__all__ = ["User", "Farm", "Crop", "Livestock", "MarketPrice", "CropProblem", "FarmProfile", "FarmPost", "FarmFollowing"]
+__all__ = ["User", "Farm", "Crop", "Livestock", "MarketPrice", "CropProblem", "FarmProfile", "FarmPost", "FarmFollowing", "UserFollowing"]
